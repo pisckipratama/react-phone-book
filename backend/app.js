698648dd9f0920fd5e7ec2115884e8d4feb4c7ec
Piscki Pratama <pisckipratama@gmail.com>
2020-03-25 01:17:19 +0700

    change mongoose to firebase and setup graphql

--- backend/app.js
+++ backend/app.js
@@ -2,31 +2,41 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const firebase = require('firebase');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
+
+const config = {
+  apiKey: "AIzaSyAuvYHZh-SjCbqQ1FZ60qW0eDWbMRyAPNY",
+  authDomain: "pisckify.firebaseapp.com",
+  databaseURL: "https://pisckify.firebaseio.com",
+  projectId: "pisckify",
+  storageBucket: "pisckify.appspot.com",
+  messagingSenderId: "269128389336"
+}
+
+firebase.initializeApp(config);
 
 const indexRouter = require('./routes/phone');
 
 const app = express();
 
+// app.use(logger('dev'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.use(cors());
+app.use('*', cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// database config
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://pisckipy:nopassword@reviewapi-a48cn.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true
-  })
-  .then(() => console.log('successfully connected with mongodb.'))
-  .catch(err => console.error(err))
-
 app.use('/api', indexRouter);
+
+const userSchema = require('./graphql').userSchema;
+app.use('/graphql', cors(), graphqlHTTP({
+  schema: userSchema,
+  rootValue: global,
+  graphiql: true
+}))
 
 module.exports = app;
